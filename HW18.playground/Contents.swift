e8e33c64ebb7e67ce26f5fe0708e401e1e57fa72
HristinaBuyanova <hristina-a@mail.ru>
2023-11-15 23:37:56 +0300

    Add class for save chip

--- HW18.playground/Contents.swift
+++ HW18.playground/Contents.swift
@@ -1,5 +1,8 @@
 import Foundation
+import PlaygroundSupport
+PlaygroundPage.current.needsIndefiniteExecution = true
 
+// MARK: - Chip.swift
 public struct Chip {
     public enum ChipType: UInt32 {
         case small = 1
@@ -13,7 +16,6 @@ public struct Chip {
         guard let chipType = Chip.ChipType(rawValue: UInt32(arc4random_uniform(3) + 1)) else {
             fatalError("Incorrect random value")
         }
-
         return Chip(chipType: chipType)
     }
 
@@ -21,4 +23,43 @@ public struct Chip {
         let soderingTime = chipType.rawValue
         sleep(UInt32(soderingTime))
     }
+}
+
+// MARK: - Realization
+
+final class SaveChip<T> {
+
+    private var array = [T]()
+    private var nsCondition = NSCondition()
+
+    var isEmpty: Bool {
+        nsCondition.lock()
+        let empty = array.isEmpty
+        nsCondition.unlock()
+        return empty
+    }
+
+    func push(_ chip: T) {
+        nsCondition.lock()
+        array.append(chip)
+        nsCondition.unlock()
+    }
+
+    func pop() -> T? {
+        nsCondition.lock()
+        let pop = array.removeLast()
+        nsCondition.unlock()
+        return pop
+    }
+}
+
+var array = [Chip]()
+
+
+final class Generate: Thread {
+
+}
+
+final class Worker: Thread {
+
 }
